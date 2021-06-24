93a783651777d4a1808efe63e5194545beb9ec26
Shahid Hasan <shahid_hasan@epam.com>
2021-06-24 19:03:59 +0530

    dev: book service for library management project

---
+++ src/main/java/com/epam/books/controllers/BookController.java
@@ -0,0 +1,84 @@
+package com.epam.books.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.epam.books.entities.Book;
+import com.epam.books.exceptions.BookExistsException;
+import com.epam.books.exceptions.BookNotFoundException;
+import com.epam.books.services.BookService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Book Management RESTful Services", description="Controller for book service")
+@RestController
+@Validated
+public class BookController {
+
+	@Autowired
+	private BookService bookService;
+
+	@ApiOperation(value = "List of all books")
+	@GetMapping(value="/books", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Book> getAllBooks(){
+		return bookService.getAllBooks();
+	}
+	@ApiOperation(value = "Add a book")
+	@PostMapping(value="/books", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> addBook(@Valid @RequestBody Book book, UriComponentsBuilder builder) throws BookExistsException {
+
+		Book addBook= bookService.addBook(book);
+		HttpHeaders headers= new HttpHeaders();
+		headers.setLocation(builder.path("/books/{book_id}").buildAndExpand(addBook.getId()).toUri());
+		return  new ResponseEntity<>(headers, HttpStatus.CREATED);
+
+	}
+
+	@ApiOperation(value = "Get book by id")
+	@GetMapping(value="/books/{book_id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Book getBookById(@PathVariable("book_id")  @Min(1) Long id){
+		try {
+			Optional<Book> optionalBook=bookService.getBookById(id);
+			return optionalBook.get();
+		} catch(BookNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+
+	}
+
+	@ApiOperation(value = "Update a book")
+	@PutMapping(value="/books/{book_id}", produces=MediaType.APPLICATION_JSON_VALUE) 
+	public Book updateBookById(@PathVariable("book_id") Long id, @RequestBody Book book) {
+		try {
+			return bookService.updateBookById(id, book);
+		} catch (BookNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
+
+	@ApiOperation(value = "Delete a book")
+	@DeleteMapping(value="/books/{book_id}")
+	public void deleteBookById(@PathVariable("book_id") Long id ) {
+		bookService.deleteBookById(id);
+	}
+}
---
+++ src/main/java/com/epam/books/entities/Book.java
@@ -0,0 +1,119 @@
+package com.epam.books.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModel;
+
+
+
+@Entity
+@Table(name="book")
+@ApiModel(description="This model is to create a book")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="BOOK_ID")
+	private Long id;
+
+
+	@Column(name="BOOK_NAME", length=100, nullable=false, unique = true)
+	@NotEmpty(message = "Book name is mandatory , please provide bookname")
+	private String bookname;
+
+	@Column(name="AUTHOR", length=50, nullable=false)
+	@Size(min=2, message="Author Name should have minimum 2 characters")
+	private String authorname;
+
+	@Column(name="CATEGORY", length=50, nullable=false)
+	private String category;
+
+	@Column(name="DESCRIPTION", length=200, nullable=false)
+	private String description;
+
+
+	@Column(name="PUBLISH_YEAR", length=4, nullable=false)
+	private int publishYear;
+
+
+	public Book() {
+
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getBookname() {
+		return bookname;
+	}
+
+
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+
+
+
+
+	public String getAuthorname() {
+		return authorname;
+	}
+
+
+	public void setAuthorname(String authorname) {
+		this.authorname = authorname;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public int getPublishYear() {
+		return publishYear;
+	}
+
+
+	public void setPublishYear(int publishYear) {
+		this.publishYear = publishYear;
+	}
+
+
+
+
+
+
+
+
+}
